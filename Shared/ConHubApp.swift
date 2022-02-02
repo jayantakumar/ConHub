583e43145b94e67236e81b09731e6cfbc04e0848
jayantakumar <benten867@gmail.com>
2022-02-02 20:13:25 +0530

    Initial Commit

---
+++ Shared/ConHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConHubApp.swift
+//  Shared
+//
+//  Created by Jayanta kumar on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ConHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
